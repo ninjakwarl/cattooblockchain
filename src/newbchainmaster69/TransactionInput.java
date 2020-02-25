1e47eff93e8eb9110d00cf55aca21ade5e09e61f
Karl Joseph Saycon <angpahayagan@yahoo.com>
2020-02-25 20:36:17 +0800

    first commit

---
+++ src/newbchainmaster69/TransactionInput.java
@@ -0,0 +1,11 @@
+/*Author: Karl Joseph Saycon*/
+package newbchainmaster69;
+
+public class TransactionInput {
+	public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+	public TransactionOutput UTXO; //Contains the Unspent transaction output
+	
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
+}
